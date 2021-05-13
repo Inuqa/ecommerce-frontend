3cb3c134a6965ca82cd7f4f5927ecfcdb9ab5d39
inuqa <seba.nrc@gmail.com>
2021-05-12 23:55:09 -0400

    added cart and home components to app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
 import Header from './components/Header';
 import Product from './pages/Product';
 import Products from './pages/Products';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 import Container from 'react-bootstrap/Container';
 import './App.css';
 
@@ -24,7 +26,11 @@ function App() {
           <Route exact path="/products">
             <Products />
           </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
           <Route path="/">
+            <Home />
           </Route>
         </Switch>
       </Container>
