5c41f4119b554098348d7a48d32d1f289e7d47a6
inuqa <seba.nrc@gmail.com>
2021-05-14 04:45:14 -0400

    added load items in app

--- src/App.js
+++ src/App.js
@@ -11,8 +11,13 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Container from 'react-bootstrap/Container';
 import './App.css';
+import {useDispatch} from 'react-redux';
+import {loadCart} from './features/cart/cartSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  dispatch(loadCart());
   return (
     <Router>
       <Container>
