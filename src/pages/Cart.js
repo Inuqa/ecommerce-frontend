aac78ce252b5489acfaef2d5c173e0666f6538e3
inuqa <seba.nrc@gmail.com>
2021-05-14 04:52:34 -0400

    showing cart contents ids

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import {removeItem, selectCart} from '../features/cart/cartSlice';
+import {
+  removeItem,
+  selectCart,
+  getProducts,
+  fetchProducts,
+} from '../features/cart/cartSlice';
 import {useDispatch} from 'react-redux';
+
 
 const Cart = () => {
   const cart = useSelector(selectCart);
+  console.log(cart);
   const dispatch = useDispatch();
   const handleRemove = (id) => {
     dispatch(removeItem(id));
   };
 
+  dispatch(fetchProducts());
+
   console.log(cart);
   return (
     <div>
-      {cart.map((item, index) => (
-        <div key={index}>
-          <img src={item.url} alt="" />
-          <h1>{item.product.title}</h1>
-          <h2>{item.product.price}</h2>
-          <p>{item.quantity}</p>
-          <img src={item.product.image} alt="" />
-          <button onClick={() => handleRemove(index)}>remove item</button>
-        </div>
-      ))}
+      {Object.entries(cart).map((item) =>
+        <div key={item}>
+          <h1>{`${item[0]} cantidad ${item[1]}`}</h1>
+        </div>,
+      )}
     </div>
   );
 };
