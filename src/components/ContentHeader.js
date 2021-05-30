426c07afa33edc4663ed3b150bfabb125c2bc1e8
inuqa <seba.nrc@gmail.com>
2021-05-30 00:43:34 -0400

    added optional btn in content header

--- src/components/ContentHeader.js
+++ src/components/ContentHeader.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-const ContentHeader = ({title}) => {
+const ContentHeader = ({title, btn, btnVal}) => {
+  console.log(btn)
   return (
-    <div className="main-header">
+    <div className="main-header d-flex justify-content-between">
       <h1>{title}</h1>
+      {btn && <Link className="btn btn-primary" to={btn}>{btnVal}</Link> }
     </div>
   );
 };
